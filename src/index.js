b3798e6ba89534162cd232dca8280f5862af28cc
alpinewebdev <alpinewebdev@gmail.com>
2017-07-27 19:35:21 +0200

    RC

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 import DOMPurify from 'dompurify'
-
 import './index.css';
 
 
@@ -12,45 +11,31 @@ class Leaderboard extends React.Component{
         super();
 
         this.state = {
-            objectMonth : {},
-            numbers : [],
-            names : [],
-            pointsMonth : [],
-            pointsAllTime : []
         }  
 
-        this.getTopAllTime();
-        this.getTopMonth();
+        this.getTop();
     }
     
 
-    resolveUsersMonth(){
-        return new Promise(function(resolve, reject){
-            fetch("https://fcctop100.herokuapp.com/api/fccusers/top/recent").then((value) => value.json()).then(function(value){resolve(value);});
-        });
-    };
-
-    getTopMonth(){
-        
-        this.state.objectMonth = this.resolveUsersMonth().then(function(value){
-                                                        return value;
-                                                      });
-    }
-
-    resolveUsersAllTime(){
+    resolveUsers(){
         return new Promise(function(resolve, reject){
             fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime").then((value) => value.json()).then((value) => resolve(value))});
     };
 
-    getTopAllTime(){
+    getTop(){
 
-        const getData = async () => {this.setState({objectAllTime : await this.resolveUsersAllTime()})
+        const getData = async () => {this.setState({objectUsers : await this.resolveUsers()})
                                      return 'done'}
-        //const setData = () => this.setState({objectAllTime : getData()});  
         getData();
 
     }
 
+    reorderUsersAllTime(){
+        this.setState({objectUsers : this.state.objectUsers.sort(function(a, b){return b.alltime - a.alltime})});
+    }
+    reorderUsersRecent(){
+         this.setState({objectUsers : this.state.objectUsers.sort(function(a, b){return b.recent - a.recent})});
+    }
    
 
     render(){
@@ -61,10 +46,10 @@ class Leaderboard extends React.Component{
                         <div className="row">
                             <div className="col-1 d-flex justify-content-center"> <b>#</b> </div>
                             <div className="col-4 d-flex justify-content-center"> <b>Camper Name</b> </div>
-                            <div className="col-3 d-flex justify-content-center"> <b>Points in past 30 days</b> </div>
-                            <div className="col-4 d-flex justify-content-center"> <b>All time points</b> </div>
+                            <div className="col-3 d-flex justify-content-center" id="reorderButtonUsersRecent" onClick={this.reorderUsersRecent.bind(this)}> <b>Points in past 30 days</b> </div>
+                            <div className="col-4 d-flex justify-content-center" id="reorderButtonUsersAllTime" onClick={this.reorderUsersAllTime.bind(this)}> <b>All time points</b> </div>
                         </div>
-                        <Table value={this.state.objectAllTime} />
+                        <Table value={this.state.objectUsers} />
                     </div>
                 </div>
             );
@@ -81,7 +66,6 @@ function Table(props){
                     renderString += "<div class='row'><div class='col-1 d-flex justify-content-center'>" +  (index + 1) + "</div><div class='col-4 d-flex justify-content-center'>" + element.username + "</div><div class='col-3 d-flex justify-content-center'>" + element.recent + "</div><div class='col-4 d-flex justify-content-center'>" + element.alltime + "</div></div>";
                 });
         renderString = DOMPurify.sanitize(renderString);
-        console.log(DOMPurify.removed);
         
         return(
             <div dangerouslySetInnerHTML={createHTML()}></div>
